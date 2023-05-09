79de68574fa8248191866f272e11dc0a5cdad4f9
dikshantdv <vyasdikshant1210@gmail.com>
2023-05-09 21:06:05 +0530

    Final commit

--- components/FilterOverlay.js
+++ components/FilterOverlay.js
@@ -7,7 +7,7 @@ import {
     Box,
     Slider,
     VStack,
-    HStack,
+    HStack, Checkbox, Button
 } from "native-base";
 import tailwind from "twrnc";
 import { Ionicons } from "@expo/vector-icons";
@@ -15,27 +15,62 @@ import Pressable from "react-native/Libraries/Components/Pressable/Pressable";
 import SliderItem from "./SliderItem";
 import emiCalculator from "../utils/emiCalculator";
 import { priceAbbr } from "../utils/priceAbbr";
+import { useDispatch } from "react-redux";
+import { setCarListData } from "../store/CarListAction";
+import { useNavigation } from "@react-navigation/native";
 
-const FilterOverlay = ({ price = 1100000 }) => {
+const FilterOverlay = () => {
+    const dispatch = useDispatch()
+    const navigation = useNavigation()
     const [showModal, setShowModal] = useState(false);
-    const [emiData, setEmiData] = useState({
-        loanAmount: (price * 0.9).toFixed(),
-        rate: 9,
-        tenure: 5,
-    });
-    const [emi, setEmi] = useState(
-        emiCalculator(emiData.loanAmount, emiData.rate, emiData.tenure)
-    );
-    useEffect(() => {
-        const timer = setTimeout(() => {
-            setEmi(emiCalculator(emiData.loanAmount, emiData.rate, emiData.tenure));
-        }, 300);
-        return () => {
-            clearTimeout(timer);
-        };
-    }, [emiData]);
-    const minDownPayment = (price * 0.1).toFixed();
-    const maxDownpayment = (price * 0.95).toFixed();
+    const [transmissionValues, setTransmissionValues] = React.useState([]);
+    const [fuelValues, setFuelValues] = React.useState([]);
+    const [maxValue, setMaxValue] = React.useState();
+    const [minValue, setMinValue] = React.useState();
+
+    const filterhandler = () => {
+        let query = null;
+        if (maxValue) { query = `price[lte]=${maxValue}` }
+        if (minValue) { query = query ? query + `&price[gte]=${minValue}` : `price[gte]=${minValue}` }
+        if (fuelValues.length > 0) {
+            if (query) {
+                query = query + '&'
+            }
+            fuelValues.forEach((el) => {
+                query = query + `fuel=${el}`
+                if (fuelValues.slice(-1) != el) {
+
+                    query = query + "&"
+                }
+            })
+        }
+        if (transmissionValues.length > 0) {
+            if (query) {
+                query = query + '&'
+            }
+            transmissionValues.forEach((el) => {
+                if (el === 'Manual') {
+                    query = query + `transmission=${el}`
+                } else {
+                    query = query + `transmission[$ne]=${el}`
+                }
+
+                if (transmissionValues.slice(-1) != el) {
+
+
+                    query = query + "&"
+                }
+            })
+        }
+        dispatch(setCarListData(query));
+        setShowModal(false)
+        navigation.navigate("CarList");
+        setMaxValue()
+        setMinValue()
+        setTransmissionValues([])
+        setFuelValues([])
+    }
+
     return (
         <>
             <Modal
@@ -52,79 +87,59 @@ const FilterOverlay = ({ price = 1100000 }) => {
                     <Modal.Body>
                         <Box alignItems="center" w="100%">
                             <VStack w="90%" maxW="300" space={2}>
-                                <Text style={tailwind`font-bold text-lg `}>
-                                    Min Price: ₹
-                                    {priceAbbr(
-                                        (price - emiData.loanAmount)
-                                            .toFixed()
-                                            .toLocaleString("en-IN")
-                                    )}
-                                </Text>
-                                <VStack>
-                                    <SliderItem
-                                        value={price - emiData.loanAmount}
-                                        max={price * 0.95}
-                                        min={price * 0.1}
-                                        step={10000}
-                                        onChange={(e) => {
-                                            setEmiData((prev) => {
-                                                return { ...prev, loanAmount: (price - e).toFixed() };
-                                            });
-                                        }}
+                                <HStack justifyContent="space-between" alignItems='center'>
+                                    <Text style={tailwind`font-bold text-lg`}>
+                                        Min Price: ₹
+                                    </Text>
+                                    <Text></Text>
+                                    <Input
+                                        value={minValue}
+                                        w="60%"
+                                        m="2"
+                                        keyboardType="number-pad"
+                                        onChangeText={setMinValue}
+                                        placeholder="Enter minimum price"
                                     />
-                                    <HStack justifyContent="space-between">
-                                        <Text>₹ {priceAbbr(minDownPayment)}</Text>
-                                        <Text>₹ {priceAbbr(maxDownpayment)}</Text>
-                                    </HStack>
-                                </VStack>
-                                <Text style={tailwind`font-bold text-base`}>
-                                    Loan Amount will be: ₹ {priceAbbr(emiData.loanAmount)}
-                                </Text>
-                                <HStack justifyContent="space-between">
-                                    <VStack w="2/5">
-                                        <Text style={tailwind`font-bold text-base `}>
-                                            Tenure: {emiData.tenure} years
-                                        </Text>
-                                        <SliderItem
-                                            value={emiData.tenure}
-                                            min={1}
-                                            max={10}
-                                            step={1}
-                                            onChange={(e) => {
-                                                setEmiData((prev) => {
-                                                    return { ...prev, tenure: e };
-                                                });
-                                            }}
-                                        />
-                                        <HStack justifyContent="space-between">
-                                            <Text>1 year</Text>
-                                            <Text>10 year</Text>
-                                        </HStack>
-                                    </VStack>
-                                    <VStack w="2/5">
-                                        <Text style={tailwind`font-bold text-base`}>
-                                            Interest: {emiData.rate}%
-                                        </Text>
-                                        <SliderItem
-                                            value={emiData.rate}
-                                            min={1}
-                                            max={15}
-                                            step={1}
-                                            onChange={(e) => {
-                                                setEmiData((prev) => {
-                                                    return { ...prev, rate: e };
-                                                });
-                                            }}
-                                        />
-                                        <HStack justifyContent="space-between">
-                                            <Text>1%</Text>
-                                            <Text>15%</Text>
-                                        </HStack>
-                                    </VStack>
+
                                 </HStack>
-                                <Text style={tailwind`font-bold text-xl`}>
-                                    ₹{priceAbbr(emi)} EMI for {emiData.tenure} years
+                                <HStack justifyContent="space-between" alignItems='center'>
+                                    <Text style={tailwind`font-bold text-lg`}>
+                                        Max Price: ₹
+                                    </Text>
+                                    <Text></Text>
+                                    <Input
+                                        value={maxValue}
+                                        w="60%"
+                                        m="2"
+                                        keyboardType="number-pad"
+                                        onChangeText={setMaxValue}
+                                        placeholder="Enter max price"
+                                    />
+                                </HStack>
+                                <Text style={tailwind`font-bold text-lg`}>
+                                    Transmission:
                                 </Text>
+                                <Checkbox.Group onChange={setTransmissionValues} value={transmissionValues} accessibilityLabel="choose numbers">
+                                    <HStack alignItems="center">
+                                        <Checkbox value="Automatic" my={2}>
+                                            Automatic
+                                        </Checkbox>
+                                        <Checkbox value="Manual">Manual</Checkbox>
+                                    </HStack>
+                                </Checkbox.Group>
+                                <Text style={tailwind`font-bold text-lg`}>
+                                    Fuel:
+                                </Text>
+                                <Checkbox.Group onChange={setFuelValues} value={fuelValues} accessibilityLabel="choose numbers">
+                                    <HStack alignItems="center">
+                                        <Checkbox value="Petrol" my={2}>
+                                            Petrol
+                                        </Checkbox>
+                                        <Checkbox value="Diesel">Diesel</Checkbox>
+
+                                    </HStack>
+                                </Checkbox.Group>
+                                <Button onPress={filterhandler}>Filter</Button>
                             </VStack>
                         </Box>
                     </Modal.Body>
